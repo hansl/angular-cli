8c598b12d0194e87f2d362bbf82d3541d7cd686d
Hans Larsen <hans@hansl.ca>
2019-02-25 22:24:33 -0800

    wip: ui

--- packages/angular_devkit/architect_cli/bin/architect.ts
+++ packages/angular_devkit/architect_cli/bin/architect.ts
@@ -17,8 +17,9 @@ import {
   schema,
   tags, terminal,
 } from '@angular-devkit/core';
-import { NodeJsSyncHost, createConsoleLogger } from '@angular-devkit/core/node';
-import { existsSync, readFileSync } from 'fs';
+import { NodeJsSyncHost } from '@angular-devkit/core/node';
+import * as blessed from 'blessed';
+import { existsSync } from 'fs';
 import * as minimist from 'minimist';
 import * as path from 'path';
 import { last } from 'rxjs/operators';
@@ -81,98 +82,266 @@ interface BarInfo {
 
 
 async function _executeTarget(
-  parentLogger: logging.Logger,
-  workspace: experimental.workspace.Workspace,
-  root: string,
-  argv: minimist.ParsedArgs,
-  registry: json.schema.SchemaRegistry,
+  screen: blessed.Widgets.Screen,
+  architect: index2.Architect,
+  target: index2.Target,
 ) {
-  const architectHost = new WorkspaceNodeModulesArchitectHost(workspace, root);
-  const architect = new index2.Architect(architectHost, registry);
+  // Create the box layout.
+  const box = blessed.box({
+    label: '[ ' + _targetStringFromTarget(target) + ' ]',
+    parent: screen,
+    draggable: true,
+    mouse: true,
+    keys: true,
+    width: '80%',
+    height: '90%',
+    border: 'line',
+    shrink: true,
+  });
+  const status = blessed.text({
+    parent: box,
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 1,
+    content: '...',
+    bg: 'blue',
+  });
+  const bar = blessed.progressbar({
+    parent: box,
+    top: 1,
+    left: 0,
+    right: 0,
+    height: 1,
+    mouse: true,
+    orientation: 'horizontal',
+    pch: '#',
+    filled: 0,
+    value: 0,
+    keys: true,
+  });
+  const log = blessed.scrollabletext({
+    parent: box,
+    top: 2,
+    bottom: -1,
+    left: -1,
+    right: -1,
+    mouse: true,
+    scrollable: true,
+    label: 'Logs output:',
+    border: 'line',
+    alwaysScroll: true,
+    scrollbar: {
+      ch: 'X',
+    },
+  });
 
-  // Split a target into its parts.
-  const targetStr = argv._.shift() || '';
-  const [project, target, configuration] = targetStr.split(':');
-  const targetSpec = { project, target, configuration };
+  const resize = blessed.box({
+    parent: box,
+    right: -1,
+    bottom: -1,
+    content: '+',
+    width: 1,
+    height: 1,
+    mouse: true,
+  });
+  let resizing = false;
+  resize.on('mousedown', () => {
+    resizing = true;
+    resize.once('mouseup', () => resizing = false);
+  });
+  screen.on('mousemove', s => {
+    if (resizing) {
+      box.position.right = s.x;
+      box.position.bottom = s.y;
+    }
+  });
 
-  delete argv['help'];
-  delete argv['_'];
+  const logger = new logging.Logger('architect');
+  logger.subscribe(entry => {
+    log.append(blessed.text({
+      content: entry.message,
+    }));
+  });
 
-  const logger = new logging.Logger('jobs');
-  const logs: logging.LogEntry[] = [];
-  logger.subscribe(entry => logs.push({ ...entry, message: `${entry.name}: ` + entry.message }));
-
-  const run = await architect.scheduleTarget(targetSpec, argv, { logger });
-  const bars = new MultiProgressBar<number, BarInfo>(':name :bar (:current/:total) :status');
-
+  const run = await architect.scheduleTarget(target, {}, { logger });
   run.progress.subscribe(
     update => {
-      const data = bars.get(update.id) || {
-        id: update.id,
-        builder: update.builder,
-        target: update.target,
-        status: update.status || '',
-        name: ((update.target ? _targetStringFromTarget(update.target) : update.builder.name)
-                + ' '.repeat(80)
-              ).substr(0, 40),
-      };
-
       if (update.status !== undefined) {
-        data.status = update.status;
+        status.setContent(update.status);
+      }
+      if (update.current && update.total) {
+        bar.setProgress((update.current / update.total * 100) | 0);
       }
 
-      switch (update.state) {
-        case index2.BuilderProgressState.Error:
-          data.status = 'Error: ' + update.error;
-          bars.update(update.id, data);
-          break;
-
-        case index2.BuilderProgressState.Stopped:
-          data.status = 'Done.';
-          bars.complete(update.id);
-          bars.update(update.id, data, update.total, update.total);
-          break;
-
-        case index2.BuilderProgressState.Waiting:
-          bars.update(update.id, data);
-          break;
-
-        case index2.BuilderProgressState.Running:
-          bars.update(update.id, data, update.current, update.total);
-          break;
-      }
-
-      bars.render();
+      box.render();
     },
   );
+  //     const data = bars.get(update.id) || {
+  //       id: update.id,
+  //       builder: update.builder,
+  //       target: update.target,
+  //       status: update.status || '',
+  //       name: ((update.target ? _targetStringFromTarget(update.target) : update.builder.name)
+  //               + ' '.repeat(80)
+  //             ).substr(0, 40),
+  //     };
+  //
+  //     if (update.status !== undefined) {
+  //       data.status = update.status;
+  //     }
+  //
+  //     switch (update.state) {
+  //       case index2.BuilderProgressState.Error:
+  //         data.status = 'Error: ' + update.error;
+  //         bars.update(update.id, data);
+  //         break;
+  //
+  //       case index2.BuilderProgressState.Stopped:
+  //         data.status = 'Done.';
+  //         bars.complete(update.id);
+  //         bars.update(update.id, data, update.total, update.total);
+  //         break;
+  //
+  //       case index2.BuilderProgressState.Waiting:
+  //         bars.update(update.id, data);
+  //         break;
+  //
+  //       case index2.BuilderProgressState.Running:
+  //         bars.update(update.id, data, update.current, update.total);
+  //         break;
+  //     }
+  //
+  //     bars.render();
+  //   },
+  // );
 
   // Wait for full completion of the builder.
-  try {
+  // try {
     const result = await run.output.pipe(last()).toPromise();
+  //
+  //   if (result.success) {
+  //     parentLogger.info(terminal.green('SUCCESS'));
+  //   } else {
+  //     parentLogger.info(terminal.yellow('FAILURE'));
+  //   }
+  //
+  //   parentLogger.info('\nLogs:');
+  //   logs.forEach(l => parentLogger.next(l));
+  //
+  //   await run.stop();
+  //   bars.terminate();
+  //
+  //   return result.success ? 0 : 1;
+  // } catch (err) {
+  //   parentLogger.info(terminal.red('ERROR'));
+  //   parentLogger.info('\nLogs:');
+  //   logs.forEach(l => parentLogger.next(l));
+  //
+  //   parentLogger.fatal('Exception:');
+  //   parentLogger.fatal(err.stack);
+  //
+  //   return 2;
+  // }
+  screen.render();
+}
 
-    if (result.success) {
-      parentLogger.info(terminal.green('SUCCESS'));
-    } else {
-      parentLogger.info(terminal.yellow('FAILURE'));
-    }
 
-    parentLogger.info('\nLogs:');
-    logs.forEach(l => parentLogger.next(l));
+async function _runProgram(
+  workspace: experimental.workspace.Workspace,
+  args: string[],
+  registry: schema.CoreSchemaRegistry,
+): Promise<number> {
+  const architectHost = new WorkspaceNodeModulesArchitectHost(workspace, workspace.root);
+  const architect = new index2.Architect(architectHost, registry);
+  let resolve: (status: number) => void;
 
-    await run.stop();
-    bars.terminate();
+  process.title = 'architect';
+  const screen = blessed.screen({
+    smartCSR: true,
+    dockBorders: true,
+    warnings: true,
+  });
 
-    return result.success ? 0 : 1;
-  } catch (err) {
-    parentLogger.info(terminal.red('ERROR'));
-    parentLogger.info('\nLogs:');
-    logs.forEach(l => parentLogger.next(l));
+  screen.key(['q', 'C-q'], () => resolve(0));
 
-    parentLogger.fatal('Exception:');
-    parentLogger.fatal(err.stack);
+  const data = workspace.listProjectNames().reduce((acc: string[][], projectName) => {
+    const targets = workspace.getProjectTargets(projectName);
 
-    return 2;
-  }
+    return [
+      ...acc,
+      ...Object.keys(targets).reduce((acc: string[][], targetName) => {
+        return [
+          ...acc,
+          [projectName, targetName, ''],
+          ...Object.keys(targets[targetName].configurations || {}).map(configName => {
+            return [projectName, targetName, configName];
+          }),
+        ];
+      }, []),
+    ];
+  }, []);
+
+  const targetsBox = blessed.listtable({
+    label: '[ Targets ]',
+    parent: screen,
+    left: 0,
+    top: 0,
+    width: 'shrink',
+    height: 20,
+    draggable: true,
+    scrollable: true,
+    invertSelected: true,
+    keys: true,
+    mouse: true,
+    vi: true,
+    style: {
+      header: {
+        fg: 'blue',
+        bold: true,
+      },
+      cell: {
+        fg: 'white',
+        selected: {
+          bg: 'blue',
+        },
+      },
+    },
+    data: [['Project', 'Target', 'Configuration'], ...data],
+    scrollbar: {
+      ch: '#',
+    },
+    border: 'line',
+  });
+
+  targetsBox.focus();
+  targetsBox.on('select', (_, i) => {
+    const selection = data[i - 1];
+    const target: index2.Target = {
+      project: selection[0],
+      target: selection[1],
+      ...(selection[2] ? { configuration: selection[2] } : 0),
+    };
+    _executeTarget(screen, architect, target);
+  });
+
+  setInterval(() => screen.render(), 100);
+  screen.render();
+
+  return await new Promise<number>(r => {
+    resolve = r;
+  })
+  .catch(err => {
+    console.error(err.stack);
+
+    return -1;
+  })
+  .then(status => {
+    // Cleanup on aisle 3.
+    screen.destroy();
+
+    return status;
+  });
 }
 
 
@@ -180,47 +349,16 @@ async function main(args: string[]): Promise<number> {
   /** Parse the command line. */
   const argv = minimist(args, { boolean: ['help'] });
 
-  /** Create the DevKit Logger used through the CLI. */
-  const logger = createConsoleLogger(argv['verbose']);
-
-  // Check the target.
-  const targetStr = argv._[0] || '';
-  if (!targetStr || argv.help) {
-    // Show architect usage if there's no target.
-    usage(logger);
-  }
-
-  // Load workspace configuration file.
-  const currentPath = process.cwd();
-  const configFileNames = [
-    'angular.json',
-    '.angular.json',
-    'workspace.json',
-    '.workspace.json',
-  ];
-
-  const configFilePath = findUp(configFileNames, currentPath);
-
-  if (!configFilePath) {
-    logger.fatal(`Workspace configuration file (${configFileNames.join(', ')}) cannot be found in `
-      + `'${currentPath}' or in parent directories.`);
-
-    return 3;
-  }
-
-  const root = dirname(normalize(configFilePath));
-  const configContent = readFileSync(configFilePath, 'utf-8');
-  const workspaceJson = JSON.parse(configContent);
-
   const registry = new schema.CoreSchemaRegistry();
   registry.addPostTransform(schema.transforms.addUndefinedDefaults);
 
+  const currentPath = normalize(process.cwd());
   const host = new NodeJsSyncHost();
-  const workspace = new experimental.workspace.Workspace(root, host);
+  const workspace = await experimental.workspace.Workspace.fromPath(host, currentPath, registry);
 
-  await workspace.loadWorkspaceFromJson(workspaceJson).toPromise();
+  // return await _executeTarget(logger, workspace, root, argv, registry);
 
-  return await _executeTarget(logger, workspace, root, argv, registry);
+  return await _runProgram(workspace, argv, registry);
 }
 
 main(process.argv.slice(2))
